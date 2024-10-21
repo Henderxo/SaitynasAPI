093a5447f1eb096d415be7a4f6f25d8cf31597ad
Henderxo <hendermann123@gmail.com>
2024-10-21 14:30:24 +0300

    Fixed: swagger not supporting multiple files

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json()) 
 
-
-app.use('/api-docs/users', swaggerUi.serve, swaggerUi.setup(usersApiSpec));
-app.use('/api-docs/comments', swaggerUi.serve, swaggerUi.setup(commentsApiSpec));
-app.use('/api-docs/developers', swaggerUi.serve, swaggerUi.setup(developersApiSpec));
-app.use('/api-docs/games', swaggerUi.serve, swaggerUi.setup(gamesApiSpec));
+//Swagger
+app.use('/api-docs/users', swaggerUi.serveFiles(usersApiSpec), swaggerUi.setup(usersApiSpec));
+app.use('/api-docs/comments', swaggerUi.serveFiles(commentsApiSpec), swaggerUi.setup(commentsApiSpec));
+app.use('/api-docs/developers', swaggerUi.serveFiles(developersApiSpec), swaggerUi.setup(developersApiSpec));
+app.use('/api-docs/games', swaggerUi.serveFiles(gamesApiSpec), swaggerUi.setup(gamesApiSpec));
 
 // User routes
 app.get('/users', usersController.getAllUsers) 
@@ -35,16 +35,13 @@ app.get('/users/:id', usersController.getUserById)
 app.post('/users', usersController.createUser) 
 app.put('/users/:id', usersController.updateUser) 
 app.delete('/users/:id', usersController.deleteUser) 
-app.get('/users/:userId/developers', usersController.getUsersDevelopers)
 
 // Game routes
-app.get('/developers/games/:developerId', gamesController.getGamesByDeveloper)
 app.get('/games', gamesController.getAllGames) 
 app.get('/games/:id', gamesController.getGameById) 
 app.post('/games', gamesController.createGame) 
 app.put('/games/:id', gamesController.updateGame) 
 app.delete('/games/:id', gamesController.deleteGame) 
-
 
 // Developer routes
 app.get('/developers', developersController.getAllDevelopers) 
@@ -52,15 +49,20 @@ app.get('/developers/:id', developersController.getDeveloperById)
 app.post('/developers', developersController.createDeveloper) 
 app.put('/developers/:id', developersController.updateDeveloper) 
 app.delete('/developers/:id', developersController.deleteDeveloper) 
-app.get('/developers/:developerId/comments', developersController.getCommentsByDeveloper);
 
 // Comment routes
 app.get('/comments', commentsController.getAllComments)
-app.get('/games/comments/:gameId', commentsController.getCommentsByGame) 
 app.get('/comments/:id', commentsController.getCommentById) 
 app.post('/comments', commentsController.createComment) 
 app.put('/comments/:id', commentsController.updateComment) 
 app.delete('/comments/:id', commentsController.deleteComment) 
+
+
+// More routes
+app.get('/users/:userId/developers', usersController.getUsersDevelopers)
+app.get('/developers/:developerId/comments', developersController.getCommentsByDeveloper);
+app.get('/developers/:developerId/games', gamesController.getGamesByDeveloper)
+app.get('/games/:gameId/comments', commentsController.getCommentsByGame) 
 
 // Start the server
 app.listen(PORT, () => {
